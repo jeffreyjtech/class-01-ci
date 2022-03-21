cec978525e15a55ffbb4a5242aa2a4cc5b8fb02e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-21 14:10:21 -0700

    Added comments to code

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -5,8 +5,9 @@ const supertest = require ('supertest');
 const app = require('../server/app.js');
 const request = supertest(app);
 
-// jest injects values into our runtime environment, and we call this special version of the runtime the "test environment"
+// jest injects values and objects into our runtime environment, and we call this special version of the runtime the "test environment"
 describe('Testing our HTTP API', () => {
+  // "it" is a method from jest, which uses the callback function in the 2nd argument to test your code.
   it('Respond with status 200 to GET request to /hello route', async () => {
     // We need async...await for the test to wait for the request to complete
     const response =  await request.get('/hello');
